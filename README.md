2bc4d0e298aff1c261da543ff696a34ca5af967f
Josh Gross <joshmgross@github.com>
2022-11-08 22:25:48 -0500

    Create initial action

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# action-template
-Template for creating actions
+# Remove from Project (Classic)
+
+An action to remove an issue from a classic/V1 project board.
+
+## Inputs
+
+- `issue-number` - The number of the issue or pull request to remove
+- `project-owner` - The organization that contains the project
+- `project-number` - The number of the project board
+- `token` - An authentication token with access to the project board and issue/pull request
+
+`issue-owner` and `issue-repository` can be specified if the issue or pull request does not exist in the workflow's repository.
+
+Use `fail-not-found: true` if you want the action to fail if the issue or pull request does not exist on the project board.
+
+## Usage
+
+### Remove an issue from a project board
+
+```yaml
+- name: Post message
+  uses: joshmgross/remove-from-project-classic@main
+  with:
+    issue-number: 1234
+    project-owner: github
+    project-number: 5678
+    token: ${{ secrets.PROJECT_TOKEN }}
+```
+
+## Not supported (yet)
+
+- User and repository project boards
+- Project cards that are not issues or pull requests
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,174 @@
 import * as core from "@actions/core";
-
-import * as utils from "./utils";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/core";
+import { graphql } from "@octokit/graphql";
 
 async function run(): Promise<void> {
-    try {
-        // Inputs and validation
-        const myInput = core.getInput("my-input");
-
-        utils.logInfo(`The specified input is ${myInput}`);
-
-        utils.logInfo("🎉🎈🎊 Action complete 🎉🎈🎊");
-    } catch (error) {
-        core.setFailed(`❌ Action failed with error: ${error}`);
+  try {
+    // Inputs and validation
+    const issueNumber = Number(core.getInput("issue-number"));
+    if (Number.isNaN(issueNumber)) {
+      throw new Error("issue-number must be a number");
     }
+
+    const token = core.getInput("token", { required: true });
+    const projectOwner = core.getInput("project-owner", { required: true });
+    const projectNumber = Number(core.getInput("project-number", { required: true }));
+
+    const currentRepository = github.context.payload.repository;
+
+    const issueOwner = core.getInput("issue-owner") || currentRepository?.owner.login;
+    const issueRepository = core.getInput("issue-repository") || currentRepository?.name;
+    if (!issueOwner || !issueRepository) {
+      throw new Error("issue-owner and issue-repository must be set, unable to determine from context");
+    }
+
+    const octokit = new Octokit({
+      auth: token
+    });
+
+    // https://docs.github.com/en/rest/issues/issues#get-an-issue
+    const issue = (
+      await octokit.request("GET /repos/{owner}/{repo}/issues/{issue_number}", {
+        owner: issueOwner,
+        repo: issueRepository,
+        issue_number: issueNumber
+      })
+    ).data;
+
+    let databaseId = issue.id;
+
+    // If the issue is a pull request, we need to get the database ID of the pull request
+    if (issue.pull_request) {
+      // https://docs.github.com/en/rest/pulls/pulls#get-a-pull-request
+      const pr = (
+        await octokit.request("GET /repos/{owner}/{repo}/pulls/{pull_number}", {
+          owner: issueOwner,
+          repo: issueRepository,
+          pull_number: issueNumber
+        })
+      ).data;
+      databaseId = pr.id;
+    }
+
+    core.info(`${issue.pull_request ? "Issue" : "Pull request"} database ID: ${databaseId}`);
+
+    const cards = await getCards(projectOwner, projectNumber, token);
+    for (const card of cards) {
+      if (card.content?.databaseId === databaseId) {
+        core.info(`Removing ${card.databaseId} from the project`);
+        // https://docs.github.com/en/rest/projects/cards#delete-a-project-card
+        await octokit.request("DELETE /projects/columns/cards/{card_id}", {
+          card_id: card.databaseId
+        });
+
+        core.info("🚀 Card removed from project 🚀");
+        return;
+      }
+    }
+
+    // Card not found
+    if (core.getBooleanInput("fail-if-not-found")) {
+      throw new Error("Card not found");
+    }
+
+    core.info("Card not found in project");
+  } catch (error) {
+    core.setFailed(`❌ Action failed with error: ${error}`);
+  }
 }
 
 run();
+
+const GET_CARDS_QUERY = `query ($login: String!, $projectNumber: Int!) {
+  organization(login: $login) {
+    name
+    project(number: $projectNumber) {
+      databaseId
+      name
+      url
+      columns(first: 10) {
+        nodes {
+          databaseId
+          name
+          cards {
+            edges {
+              node {
+                databaseId
+                content {
+                  __typename
+                  ... on Issue {
+                    databaseId
+                    number
+                  }
+                  ... on PullRequest {
+                    databaseId
+                    number
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
+
+interface OrgWithProject {
+  organization: {
+    name: string;
+    project: {
+      databaseId: number;
+      name: string;
+      url: string;
+      columns: {
+        nodes: [
+          {
+            databaseId: number;
+            name: string;
+            cards: {
+              edges: [
+                {
+                  node: Card;
+                }
+              ];
+            };
+          }
+        ];
+      };
+    };
+  };
+}
+
+interface Card {
+  databaseId: number;
+  content: {
+    __typename: string;
+    databaseId: number;
+    number: number;
+  };
+}
+
+async function getCards(owner: string, project: number, token: string): Promise<Card[]> {
+  const graphqlWithAuth = graphql.defaults({
+    headers: {
+      authorization: `token ${token}`
+    }
+  });
+  const resp: OrgWithProject = await graphqlWithAuth(GET_CARDS_QUERY, {
+    login: owner,
+    projectNumber: project
+  });
+
+  core.info(`Project: ${resp.organization.project.name}`);
+
+  const cards: Card[] = [];
+  for (const column of resp.organization.project.columns.nodes) {
+    for (const card of column.cards.edges) {
+      cards.push(card.node);
+    }
+  }
+
+  return cards;
+}
